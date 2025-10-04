f7cc7ef54a9f066baa988f7d7e66ad8d83b47009
nattthhy <natyguelcrazy@gmail.com>
2025-10-04 07:01:28 -0300

    Add files via upload

---
+++ carrinho.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>carrinho mcdonalds</title>
+	<link rel="stylesheet"  href="carrinho.css">
+
+</head>
+<body>
+<h2>carrinho</h2>
+<div class="carrinho-container">
+
+
+<?php
+    
+    $items  = array(['imagem' =>'vermelho.jpeg','preco'=>'200'],
+ 	  ['imagem' =>'amarelo.jpeg','preco'=>'100'],
+ 	  ['imagem' =>'prof.jpeg','preco'=>'300'] );
+
+
+    foreach ($items as $key => $value) {
+   
+?>
+<div class="produto">
+	<img src="<?php echo $value['imagem']?>"/>
+	<a href="?Adicionar=<?php echo $key ?>">Adicionar ao carrinho</a>
+</div>
+<?php
+    }
+?>
+</div>
+</body>
+</html>
