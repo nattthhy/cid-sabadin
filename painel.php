8bf193dd9055579cdb7efcf7780a678b7981962b
nattthhy <natyguelcrazy@gmail.com>
2025-10-04 06:57:15 -0300

    Add files via upload

---
+++ painel.php
@@ -0,0 +1,16 @@
+<?php 
+include("protecao.php");
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>painel</title>
+</head>
+<body>
+bem vindo ao painel,<?php echo $_SESSION["nome"]; ?>
+
+
+</body>
+</html>
